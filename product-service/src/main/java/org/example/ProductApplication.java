a53681ded87cbc6be4fd84d8b05af90fea493ca2
wudongsheng <137436896@qq.com>
2020-12-08 15:39:59 +0800

    配置中心

--- product-service/src/main/java/org/example/ProductApplication.java
+++ product-service/src/main/java/org/example/ProductApplication.java
@@ -3,10 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-// 开启 @EnableDiscoveryClient 注解，当前版本默认会开启该注解
-@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ProductApplication {
     public static void main(String[] args) {
--- product-service/src/main/java/org/example/controller/ProductController.java
+++ product-service/src/main/java/org/example/controller/ProductController.java
@@ -3,18 +3,49 @@ package org.example.controller;
 import org.example.pojo.Product;
 import org.example.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@RefreshScope
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @Value("${coupon.name:}")
+    private String productName;
+
+    @Value("${project.name:}")
+    private String projectName;
+
+    @Value("${project.org:}")
+    private String projectOrg;
+
+    @Value("${jdbc.name:}")
+    private String jdbcName;
+
+    @Value("${ext.name:}")
+    private String extName;
+
+    @GetMapping("/config")
+    public Map<String, Object> getConfig() {
+        Map<String, Object> configMap = new HashMap();
+        configMap.put("productName", productName);
+        configMap.put("projectName", projectName);
+        configMap.put("projectOrg", projectOrg);
+        configMap.put("jdbcName", jdbcName);
+        configMap.put("extName", extName);
+        return configMap;
+    }
 
     /**
      * 查询商品列表
